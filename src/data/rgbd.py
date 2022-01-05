3dcfa5ccb0263edae14028cca73ccd4e32bfc9ca
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-01-05 15:13:24 -0300

    WIP: add depth scale option

--- src/data/rgbd.py
+++ src/data/rgbd.py
@@ -4,13 +4,14 @@ import open3d as o3d
 from pathlib import Path
 
 
-def load_rgbd(image_fpath: Path):
+def load_rgbd(image_fpath: Path, depth_scale=711.1111111 / 0.05):
     image = o3d.io.read_image(str(image_fpath))
     image_data = np.asarray(image)
 
-    color_image = o3d.geometry.Image(image_data[:,:,2].astype('uint8'))
-    depth_image = o3d.geometry.Image(255-image_data[:,:,1].astype('uint8'))
+    color_image = o3d.geometry.Image((image_data[:,:,2] / 255).astype('float32'))
+    # depth_image = o3d.geometry.Image((1.371349 - 0.928849 - (1.371349 - 0.928849) * np.power(image_data[:,:,1].astype(float), 2) / np.square(255)).astype('float32'))
+    depth_image = o3d.geometry.Image((1.371349 - 0.928849 - (1.371349 - 0.928849) * image_data[:,:,1].astype(float) / 255).astype('float32'))
 
     return o3d.geometry.RGBDImage.create_from_color_and_depth(
-        color_image, depth_image, depth_scale=1
+        color_image, depth_image, depth_scale=depth_scale, depth_trunc=711.1111111 / 0.0005
     )
