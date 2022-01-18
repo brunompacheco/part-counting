907e6cc07112ccf880c92adc5e5e8c7a06c518af
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-01-18 10:59:04 -0300

    add normalization to dataset

--- src/data/base.py
+++ src/data/base.py
@@ -1,11 +1,14 @@
 """Fundamental structures and utils.
 """
-import h5py
-
 from pathlib import Path
-import numpy as np
-from torch.utils.data import Dataset
 from typing import Tuple, Union
+
+import h5py
+import numpy as np
+
+from torch import Tensor
+from torch.utils.data import Dataset
+from torchvision import transforms as T
 
 class FimacDataset(Dataset):
     def __init__(self, hdf5_fpath: Union[Path, str],
@@ -14,14 +17,24 @@ class FimacDataset(Dataset):
 
         self._data = h5py.File(str(hdf5_fpath), "r")[hdf5_dataset_name]
 
+        self.transform = T.Compose([
+            T.ToTensor(),
+            # normalize required for pre-trained image models,
+            # check https://pytorch.org/vision/stable/models.html
+            T.Normalize(mean=[0.485, 0.456], std=[0.229, 0.224]),
+        ])
+
     def __len__(self) -> int:
         return self._data.shape[0] * self._data.shape[1]
 
-    def __getitem__(self, index) -> Tuple[np.ndarray, int]:
+    def __getitem__(self, index) -> Tuple[Tensor, int]:
         i = index // self._data.shape[1]  # render index
         j = index % self._data.shape[1]  # render step (# of parts) index
 
         image = self._data[i,j,:,:,:]
+        # torchvision's normalize expects numpy array in shape (H x W x C)
+        image = self.transform(np.moveaxis(image, 0, -1))
+
         label = j
 
         return image, label
