a928ebf8e225e983fbb1a640afde24fc79e091ca
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-01-18 11:21:21 -0300

    implement simple NN for testing

---
+++ src/models/base.py
@@ -0,0 +1,39 @@
+"""Network definitions.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TestNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # two basic convolution layers and two fully-connected for regression
+        self.conv1 = nn.Conv2d(2, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.fc1 = nn.Linear(4129024, 128)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+
+        # Run max pooling over x
+        x = F.max_pool2d(x, 2)
+        # Pass data through dropout1
+        x = self.dropout1(x)
+        # Flatten x with start_dim=1
+        x = torch.flatten(x, 1)
+
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        return x
