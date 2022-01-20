23d367ee060ee554b4ab7a5c83ce7c6093e49daa
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-01-20 10:56:13 -0300

    implement continue/resuming

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -15,6 +15,7 @@ from torch.cuda.amp import autocast, GradScaler
 
 from src.data import FimacDataset
 from src.models import TestNet
+from src.models.base import get_model_class
 
 # find .env automagically by walking up directories until it's found
 dotenv_path = find_dotenv()
@@ -90,20 +91,113 @@ def validation_pass(net, dataloader, criterion):
     len_data = len(dataloader.dataset)
     return val_loss / len_data, val_MAE / len_data, val_acc / len_data
 
+def timeit(fun):
+    def fun_(*args, **kwargs):
+        start_time = time()
+        f_ret = fun(*args, **kwargs)
+        end_time = time()
+
+        return end_time - start_time, f_ret
+
+    return fun_
+
 @click.command()
 @click.argument('epochs', type=click.INT)
-@click.option('--frac', type=click.FLOAT, default=1.0,
-              help='Fraction of the data to be used (train + validation).')
-def main(epochs, frac):
+@click.option('--frac', type=click.FLOAT, default=1.0, help=(
+    'Fraction of the data to be used (train + validation). This option is '
+    'overwritten if a checkpoint is loaded.'
+))
+@click.option('--checkpoint', type=click.Path(exists=True),
+              help='Path to the checkpoint file.')
+def main(epochs, frac, checkpoint):
     """Train a model in the Fimac render dataset.
     """
     logger = logging.getLogger(__name__)
 
-    # training hyperparameters
-    batch_size = 16
-    lr = 0.001
-    criterion = nn.L1Loss()
+    if checkpoint is not None:
+        checkpoint_fpath = checkpoint
+        chk_name = Path(checkpoint_fpath).name
+        run_name, model_name, _ = chk_name.replace('.tar', '').split('__')
+        checkpoint = torch.load(checkpoint_fpath)
 
+        config = checkpoint['config']
+
+        epoch = checkpoint['epoch'] + 1
+
+        logger.info(f'Resuming training of {run_name} at epoch {epoch}')
+
+        # maybe I'll have to change this for lr decay techniques, but that's
+        # a problem for the future
+        epochs += epoch
+        config['epochs'] = epochs
+
+        # load hyperparameters
+        batch_size = config['batch_size']
+        lr = config['learning_rate']
+        frac = config['data_fraction']
+        batch_size = config['batch_size']
+
+        criterion = eval(f"nn.{config['loss_func']}()")
+
+        net = get_model_class(model_name)().to(device)
+        net.load_state_dict(checkpoint['model_state_dict'])
+
+        optimizer = eval(f"torch.optim.{config['optimizer']}")
+        optimizer = optimizer(net.parameters(), lr=lr)
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        # initialize wandb
+        wandb.init(
+            project="part-counting",
+            entity="brunompac",
+            id=checkpoint['wandb_run_id'],
+            resume=True,
+        )
+        wandb.watch(net)
+    else:
+        logger.info('Setting up training')
+
+        epoch = 0
+
+        # hyperparameters
+        batch_size = 16
+        lr = 0.001
+        criterion = nn.L1Loss()
+
+        net = TestNet().to(device)
+
+        optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+
+        config = {
+            "learning_rate": lr,
+            "data_fraction": frac,
+            "epochs": epochs,
+            "batch_size": batch_size,
+            "model": type(net).__name__,
+            "optimizer": type(optimizer).__name__,
+            "loss_func": type(criterion).__name__,
+        }
+
+        # initialize wandb
+        wandb.init(
+            project="part-counting",
+            entity="brunompac",
+            config=config,
+        )
+        wandb.watch(net)
+
+        checkpoint_fpath = project_dir/(
+            "models/"
+            f"{wandb.run.name}__{config['model']}__checkpoint.tar"
+        )
+
+    logger.info(f"Training {config['model']} model")
+    logger.info(f"optimizer = {config['optimizer']}")
+    logger.info(f"learning rate = {config['learning_rate']}")
+    logger.info(f"batch size = {config['batch_size']}")
+    logger.info(f"loss function = {config['loss_func']}")
+
+    logger.info('Preparing data')
     dataset = FimacDataset(project_dir/'data/interim/renders.hdf5')
 
     # get fraction of the data
@@ -118,79 +212,60 @@ def main(epochs, frac):
     )
 
     # instantiate DataLoaders
-    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=batch_size,
+                                  shuffle=True)
     val_dataloader = DataLoader(val_data, batch_size=32)
 
-    # instantiate model
-    net = TestNet().to(device)
-
-    # more "hyperparameters"
-    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
     scaler = GradScaler()
 
-    config = {
-        "learning_rate": lr,
-        "data_fraction": frac,
-        "epochs": epochs,
-        "batch_size": batch_size,
-        "model": type(net).__name__,
-        "optimizer": type(optimizer).__name__,
-        "loss_func": type(criterion).__name__,
-    }
-
-    logger.info(f"Training {config['model']} model")
-    logger.info(f"optimizer = {config['optimizer']}")
-    logger.info(f"learning rate = {config['learning_rate']}")
-    logger.info(f"batch size = {config['batch_size']}")
-    logger.info(f"loss function = {config['loss_func']}")
-
-    # initialize wandb
-    wandb.init(
-        project="part-counting",
-        entity="brunompac",
-        config=config,
-    )
-
-    wandb.watch(net)
-
-    for epoch in range(epochs):
-        logger.info(f"Started epoch {epoch+1}/{epochs}")
+    while epoch < epochs:
+        logger.info(f"Epoch {epoch} started")
         epoch_start_time = time()
 
         # train
-        start_time = time()
-        train_loss = train_pass(net, train_dataloader, criterion, optimizer,
-                                scaler)
-        end_time = time()
+        train_time, train_loss = timeit(train_pass)(net, train_dataloader,
+                                                    criterion, optimizer,
+                                                    scaler)
 
-        logger.info(f"Training pass took {end_time - start_time:.3f} seconds")
+        logger.info(f"Training pass took {train_time:.3f} seconds")
         logger.info(f"Training loss = {train_loss}")
 
-        wandb.log({
-            "train_loss": train_loss,
-        }, step=epoch)
-
         # validation
-        start_time = time()
-        val_loss, val_MAE, val_acc = validation_pass(net, val_dataloader, criterion)
-        end_time = time()
+        val_time, (val_loss, val_MAE, val_acc) = timeit(validation_pass)(
+            net,
+            val_dataloader,
+            criterion,
+        )
 
-        logger.info(f"Validation pass took {end_time - start_time:.3f} seconds")
+        logger.info(f"Validation pass took {val_time:.3f} seconds")
         logger.info(f"Validation loss = {val_loss}")
         logger.info(f"Validation MAE = {val_MAE}")
         logger.info(f"Validation accuracy = {100 * val_acc:.2f} %")
 
         wandb.log({
+            "train_loss": train_loss,
             "val_loss": val_loss,
             "val_MAE": val_MAE,
             "val_acc": val_acc,
         }, step=epoch, commit=True)
 
+        logger.info(f"Saving checkpoint at {checkpoint_fpath}.")
+        checkpoint = {
+            'config': config,
+            'epoch': epoch,
+            'wandb_run_id': wandb.run.id,
+            'model_state_dict': net.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }
+        torch.save(checkpoint, checkpoint_fpath)
+
         epoch_end_time = time()
         logger.info(
-            f"Epoch {epoch+1} finished and took "
+            f"Epoch {epoch} finished and took "
             f"{epoch_end_time - epoch_start_time:.2f} seconds"
         )
+
+        epoch += 1
 
     model_fpath = project_dir/f"models/{wandb.run.name}__{config['model']}.pth"
     torch.save(net.state_dict(), model_fpath)
