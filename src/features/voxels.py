abe79cc6a4c630756a87e227482d25bfaf79d98f
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-03-01 20:41:35 -0300

    make voxel grid image from point cloud

---
+++ src/features/voxels.py
@@ -0,0 +1,54 @@
+import numpy as np
+import open3d as o3d
+
+
+def get_shape_pos(v_grid: o3d.geometry.VoxelGrid):
+    # get grid shape and position
+    grid_pos = v_grid.get_min_bound()
+    # grid_pos[-1] = 0.0  # floor
+    grid_pos[-1] = 0.1288  # floor
+
+    grid_shape = (v_grid.get_max_bound() - grid_pos) / v_grid.voxel_size
+    grid_shape = (grid_shape + 0.5).astype(int)
+
+    return grid_shape, grid_pos
+
+def voxel2np(v_grid: o3d.geometry.VoxelGrid, pos: np.array, shape: np.array,
+             is_floating=False):
+    """Convert VoxelGrid to numpy array.
+
+    Offsets the voxels in `v_grid` to match a grid at `pos` with `shape`.
+
+    Args:
+        v_grid: Open3D VoxelGrid to be converted.
+        pos: Spatial positioning of the corner of the final grid, its minimum
+        bound.
+        shape: Shape of the target grid.
+        is_floating: If the object voxelized is floating (default `False`). In
+        case it is, doesn't fill to the bottom of the grid, but fills the
+        middle (in the z direction).
+    
+    Returns:
+        grid: Numpy voxel grid.
+    """
+    grid = np.zeros(shape, dtype=bool)
+    
+    offset = (pos - v_grid.get_min_bound()) / v_grid.voxel_size
+    offset = (offset + 0.5).astype(int)
+
+    indices = [v.grid_index - offset for v in v_grid.get_voxels()]
+
+    for i in indices:
+        if (i > 0).all() and (i < shape).all():
+            grid[i[0],i[1],0:i[2]] = True
+
+    if is_floating:
+        grid_floating = np.zeros(shape, dtype=bool)
+
+        for i in indices:
+            if (i > 0).all() and (i < shape).all():
+                grid_floating[i[0],i[1],i[2]:-1] = True
+
+        grid = grid & grid_floating
+
+    return grid
