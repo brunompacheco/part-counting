5c9c2754d13d579801237300824bf796d6c53478
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-03-01 21:31:05 -0300

    add new mode of generating the grid

--- src/features/voxels.py
+++ src/features/voxels.py
@@ -14,7 +14,7 @@ def get_shape_pos(v_grid: o3d.geometry.VoxelGrid):
     return grid_shape, grid_pos
 
 def voxel2np(v_grid: o3d.geometry.VoxelGrid, pos: np.array, shape: np.array,
-             is_floating=False):
+             mode='shadow'):
     """Convert VoxelGrid to numpy array.
 
     Offsets the voxels in `v_grid` to match a grid at `pos` with `shape`.
@@ -24,31 +24,34 @@ def voxel2np(v_grid: o3d.geometry.VoxelGrid, pos: np.array, shape: np.array,
         pos: Spatial positioning of the corner of the final grid, its minimum
         bound.
         shape: Shape of the target grid.
-        is_floating: If the object voxelized is floating (default `False`). In
-        case it is, doesn't fill to the bottom of the grid, but fills the
-        middle (in the z direction).
-    
+        mode: how to fill the voxel grid based on `v_grid` voxels. If `shadow`
+        (default), fills everything below. If `reverse-shadow`, fills
+        everything above. If `floating`, fills only the intersection between
+        `shadow` and `reverse-shadow` grids, that is, the middle in the z axis.
+
     Returns:
         grid: Numpy voxel grid.
     """
-    grid = np.zeros(shape, dtype=bool)
-    
     offset = (pos - v_grid.get_min_bound()) / v_grid.voxel_size
     offset = (offset + 0.5).astype(int)
 
     indices = [v.grid_index - offset for v in v_grid.get_voxels()]
 
+    # image filled to the bottom
+    grid_bottom = np.zeros(shape, dtype=bool)
     for i in indices:
         if (i > 0).all() and (i < shape).all():
-            grid[i[0],i[1],0:i[2]] = True
+            grid_bottom[i[0],i[1],0:i[2]] = True
 
-    if is_floating:
-        grid_floating = np.zeros(shape, dtype=bool)
+    # image filled to the top
+    grid_top = np.zeros(shape, dtype=bool)
+    for i in indices:
+        if (i > 0).all() and (i < shape).all():
+            grid_top[i[0],i[1],0:i[2]] = True
 
-        for i in indices:
-            if (i > 0).all() and (i < shape).all():
-                grid_floating[i[0],i[1],i[2]:-1] = True
-
-        grid = grid & grid_floating
-
-    return grid
+    if mode == 'shadow':
+        return grid_bottom
+    elif mode == 'floating':
+        return grid_bottom & grid_top
+    elif mode == 'reverse-shadow':
+        return grid_top
