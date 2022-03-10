76f01d3e6803c0ee821c013e069675b7af30a52c
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-03-10 16:22:31 -0300

    predict parts in box using PSO

--- src/models/pso.py
+++ src/models/pso.py
@@ -1,7 +1,8 @@
 import numpy as np
 import open3d as o3d
+import pyswarms as ps
 
-from src.features.voxels import voxel2np
+from src.features.voxels import voxel2np, dig_box
 
 
 def intersection_value(box_grid: np.ndarray, part_grid: np.ndarray):
@@ -51,3 +52,89 @@ def eval_position(x: np.array, box_grid: np.ndarray,
     part_grid = voxel2np(part_voxel, grid_pos, grid_shape, mode='floating')
 
     return - reward(box_grid, part_grid)
+
+def objective_f(xs, box_grid, part, voxel_size, grid_pos, grid_shape):
+    if xs.size > 0:
+        return np.apply_along_axis(
+            eval_position, -1, xs,
+            box_grid=box_grid,
+            part=part,
+            voxel_size=voxel_size,
+            grid_pos=grid_pos,
+            grid_shape=grid_shape
+        )
+    else:
+        return np.apply_along_axis(
+            eval_position, -1, np.array([0.,0.,0.,0.,0.,0.]),
+            box_grid=box_grid,
+            part=part,
+            voxel_size=voxel_size,
+            grid_pos=grid_pos,
+            grid_shape=grid_shape
+        )
+
+def dig_and_predict(box: o3d.geometry.PointCloud, part: o3d.geometry.PointCloud,
+                    voxel_size: float) -> int:
+    box_voxel = o3d.geometry.VoxelGrid.create_from_point_cloud(box, voxel_size)
+
+    # get grid shape and position
+    grid_pos = box_voxel.get_min_bound()
+    grid_pos[-1] = 0.1288  # floor
+
+    grid_shape = (box_voxel.get_max_bound() - grid_pos) / box_voxel.voxel_size
+    grid_shape = (grid_shape + 0.5).astype(int)
+
+    # create box grid
+    box_grid = voxel2np(box_voxel, grid_pos, grid_shape)
+    
+    # optimization bounds
+    min_bound = np.array([-0.5, -0.5, -0.5, -np.pi, -np.pi, -np.pi])
+    bounds = (min_bound, -min_bound)
+
+    ## PSO LOOP
+    n = 0  # number of parts
+    while True:
+        optimizer = ps.single.LocalBestPSO(
+            n_particles=18,
+            dimensions=6,
+            bounds=bounds,
+            options={'c1': 0.5, 'c2': 0.05, 'w':0.9, 'k': 2, 'p': 2},
+        )
+
+        _, pos = optimizer.optimize(
+            objective_f,
+            iters=500,
+            n_processes=6,
+            box_grid=box_grid,
+            part=_PointCloudTransmissionFormat(part),
+            voxel_size=voxel_size,
+            grid_pos=grid_pos,
+            grid_shape=grid_shape,
+            verbose=False,
+        )
+
+        # get part at optimal position
+        tx, ty, tz, rx, ry, rz = pos
+
+        part_ = o3d.geometry.PointCloud(part)
+
+        part_.translate(np.array([tx, ty, tz]))
+
+        part_.rotate(
+            o3d.geometry.get_rotation_matrix_from_xyz(np.array([rx, ry, rz]))
+        )
+
+        part_voxel = o3d.geometry.VoxelGrid.create_from_point_cloud(part_, voxel_size)
+        part_grid = voxel2np(part_voxel, grid_pos, grid_shape, mode='floating')
+
+        # find out how much of the part is immersed at the box content
+        immersion_ratio = (part_grid & box_grid).sum() / part_grid.sum()
+
+        if immersion_ratio > 0.60:
+            box_grid = dig_box(box_grid, part, grid_pos, grid_shape,
+                                    pos, voxel_size)
+            n += 1
+        else:
+            break
+
+    return n
